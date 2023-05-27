f9a7a81472bbacb79a45df8eae8afb7421f27bc1
IhorMykhno <mixnoiv@gmail.com>
2023-05-27 13:58:44 +0300

    feat(cleat): create base pages look
    - create editorconfig file
    - remove automatically created components
    - create default pages

---
+++ client/src/views/components/NavBar/index.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Stack, Tab, Tabs, Typography } from "@mui/material";
+
+export const NavBar = () => {
+    const [menuTub, setMenuTub] = useState(0);
+    const navigate = useNavigate();
+
+    const onChange = (e, tub) => {
+        setMenuTub(tub);
+    };
+
+    return (
+        <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            px={"30px"}
+            py={"10px"}
+        >
+            <Stack direction={"row"} alignItems={"center"}>
+                <Typography variant="h4" pr={"20px"}>
+                    QM
+                </Typography>
+                <Tabs value={menuTub} onChange={onChange}>
+                    <Tab label="Statistics" onClick={() => navigate("/")} />
+                    <Tab label="Tests" onClick={() => navigate("/tests")} />
+                    <Tab
+                        label="Create test"
+                        onClick={() => navigate("/create-test")}
+                    />
+                </Tabs>
+            </Stack>
+            <Avatar>IM</Avatar>
+        </Stack>
+    );
+};
---
+++ client/src/views/pages/Statistics/index.jsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { PageWrapper } from "../../components/PageWrapper";
+
+export const StatisticsPage = () => {
+    return (
+        <PageWrapper>
+            <Typography>Hello Statistics</Typography>
+        </PageWrapper>
+    );
+};
