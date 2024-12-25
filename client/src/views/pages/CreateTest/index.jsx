40b649780f7c3808914b5af59a9892fe93f58aca
ihormy <ihormy@softwareplanetgroup.com>
2024-12-25 20:48:05 +0200

    refactor: rename components

--- client/src/views/pages/CreateTest/index.jsx
+++ client/src/views/pages/CreateTest/index.jsx
@@ -1,10 +1,10 @@
-import { CreateTestComponent } from "@components/CreateTest";
+import { CreateNewQuiz } from "@components/Quiz/CreateNewQuiz";
 import { PageWrapper } from "@components/Wrappers/PageWrapper";
 
 export const CreateTestPage = () => {
     return (
         <PageWrapper>
-            <CreateTestComponent />
+            <CreateNewQuiz />
         </PageWrapper>
     );
 };
